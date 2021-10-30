28c33eb5cde94ba1a898d23f86d2154c685ecef5
yawa <yawsarfo2019@gmail.com>
2021-10-30 21:08:16 +0000

    day3

---
+++ src/AppNavigator.js
@@ -0,0 +1,23 @@
+import { createStackNavigator } from 'react-navigation';
+import HomeScreen from './views/Home';
+import DetailsScreen from './views/Details';
+
+
+const AppNavigator = createStackNavigator(
+    {
+        Home: {
+            screen: HomeScreen,
+            navigationOptions: { title: 'Home' }
+        },
+        Details: {
+            screen: DetailsScreen,
+            navigationOptions: { title: 'Details' }
+        }
+    },
+    {
+        initialRouteName: "Home"
+    }
+);
+
+
+export default AppNavigator;
